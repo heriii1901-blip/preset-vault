ad7d3020c552fda7b303a44510ccb3cfb2589bd3
heriii1901-blip <heriii1901@gmail.com>
2026-07-24 15:01:00 +0700

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -1,6 +1,5 @@
 import { NavLink, useLocation, useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
-import { NavLink, useLocation, useNavigate } from 'react-router-dom'
 
 export function BottomNav() {
   const location = useLocation()
