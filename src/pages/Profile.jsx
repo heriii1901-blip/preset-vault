7c7dd1adceae6e0f2dc3c0a9168998e34743196b
heriii1901-blip <heriii1901@gmail.com>
2026-07-26 00:23:35 +0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
 import { supabase } from '../supabase'
-import { BottomNav } from '../components/BottomNav'
 
 export default function Profile() {
   const { user, logout } = useAuth()
