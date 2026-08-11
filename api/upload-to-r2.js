bcc803baf87af52eaab59709ee1f9d3bbf681388
heriii1901-blip <heriii1901@gmail.com>
2026-08-11 10:03:02 +0700

    Update upload-to-r2.js

--- api/upload-to-r2.js
+++ api/upload-to-r2.js
@@ -34,7 +34,8 @@ export default async function handler(req, res) {
       return res.status(400).json({ error: "Missing x-file-name header" });
     }
 
-    const key = `presets/${Date.now()}-${fileName}`;
+    const folder = req.headers["x-upload-folder"] || "presets";
+    const key = `${folder}/${Date.now()}-${fileName}`;
 
     await s3.send(
       new PutObjectCommand({
