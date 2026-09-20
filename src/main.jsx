fb4139f1ad1f6c4569a8d4768c240b58df29e1c0
heriii1901-blip <heriii1901@gmail.com>
2026-09-20 11:40:38 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles/global.css'
+import { initTheme } from './utils/theme'
+
+// Dipasang sebelum render biar gak sempet kedip tema lama
+initTheme()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
