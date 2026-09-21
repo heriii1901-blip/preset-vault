0f8f14fcfb9522bc69205a4fee8322f729eff344
heriii1901-blip <heriii1901@gmail.com>
2026-09-21 08:05:59 +0700

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -660,7 +660,7 @@ export default function Profile() {
             <button
               type="button"
               className="profile-menu-item"
-              onClick={() => window.open('https://sl1nk.com/pam-v1-0-0')}
+              onClick={() => window.open('https://drive.google.com/file/d/1kir7PWZMzPb7ob7ETLtRuaGyWRh0GiT6/view?usp=drive_link')}
             >
               <svg className="profile-menu-icon" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
