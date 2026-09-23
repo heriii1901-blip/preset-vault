d7c850757b81db457083b3a867daebaca0dbacee
yanto kata temen <heriii1901@gmail.com>
2026-09-23 20:37:22 +0700

    Update DownloadEfek.jsx

--- src/pages/efek/DownloadEfek.jsx
+++ src/pages/efek/DownloadEfek.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate, useParams, useLocation } from 'react-router-dom'
-import { supabase } from '../supabase'
+import { supabase } from '../../supabase'
 
 export default function DownloadEfek() {
   const { effectId } = useParams()
